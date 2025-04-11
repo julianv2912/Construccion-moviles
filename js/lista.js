439045be9b6ee7a6fd60e7927dc202f8ad18a93f
julianv2912 <july2villa9@gmail.com>
2025-04-11 02:19:09 +0000

    Actualizacion lista de chatgpt

--- js/lista.js
+++ js/lista.js
@@ -1,11 +1,16 @@
-function mostrarLista(listaPokes){
+function mostrarLista(listaPokes) {
     const app = document.getElementById("app");
     let Pokes = "";
 
-    for(let i =0; i < listaPokes.length; i++){
+    for (let i = 0; i < listaPokes.length; i++) {
         let id = listaPokes[i].url.split("/")[6];
-        Pokes += 
-        <div class="c-lista-pokeon poke-$(id)" onclick="mostrarDetalle('${pokemones[i]})"
+        Pokes += `
+            <div class="c-lista-pokemon poke-${id}" onclick="mostrarDetalle('${listaPokes[i].name}')">
+                <p>#${id}</p>
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png" width="auto" height="60" loading="lazy" alt="${listaPokes[i].name}">
+                <p>${listaPokes[i].name}</p>
+            </div>
+        `;
     }
 
     app.innerHTML = Pokes;
